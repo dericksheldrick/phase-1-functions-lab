f85ea66164bd60cc2581047660f5361313ca597d
Derick Sheldrick <sheldrickderick9@gmail.com>
2025-03-07 12:24:09 +0300

    functions-lab submission

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Code your solution in this file!
+let NumberOfBlock = 42;
+
+function distanceFromHqInBlocks(block){
+     //check the number of blocks from HQ to your location
+    return Math.abs(block - NumberOfBlock);// ensure positive value is returned
+
+}
+
+function distanceFromHqInFeet(block){
+    //check the distance from HQ to your location in terms of feets
+    let blocks = distanceFromHqInBlocks(block);
+    return blocks * 264;
+}
+
+function  distanceTravelledInFeet(start, destination){
+
+    let numberOfBlockTravelled = Math.abs(destination - start);
+
+    let totalDistanceTravelled =  numberOfBlockTravelled * 264;
+
+    return totalDistanceTravelled;
+}
+
+function calculatesFarePrice(start, destination){
+
+    let distance = distanceTravelledInFeet(start,destination);
+
+    if (distance <= 400){
+        return 0;
+
+    } else if (distance <= 2000){
+        let fare = ((distance-400) * 0.02);
+        return fare;
+
+    } else if (distance <= 2500){
+        return 25;
+    }
+    else{
+        return "cannot travel that far"
+    }
+
+}
